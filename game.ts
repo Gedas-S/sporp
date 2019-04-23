606a94f1bd167a0303192cb15b65f5ee5a1b3d35
Gedas <g.simaitis@gmail.com>
2019-04-24 01:20:31 +0300

    create test sphere

--- game.ts
+++ game.ts
@@ -1,27 +1,30 @@
-// do a setinterval for game logic loop
-// with a while loop to run several times if timeout longer than expected
-// (capped, so if paused, doesn't halt everything for ages)
-
-
 class Game {
     private _canvas: HTMLCanvasElement;
     private _engine: BABYLON.Engine;
     private _scene: BABYLON.Scene;
     private _camera: BABYLON.FreeCamera;
     private _light: BABYLON.Light;
+    private _physicsEngine: BABYLON.OimoJSPlugin;
 
     constructor(canvasElement : string) {
         // Create canvas and engine.
         this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
-        this._engine = new BABYLON.Engine(this._canvas, true);
+        this._engine = new BABYLON.Engine(this._canvas, true, {
+            deterministicLockstep: true,
+            lockstepMaxSteps: 4
+        });
+        this._physicsEngine = new BABYLON.OimoJSPlugin();
+        this._physicsEngine.setTimeStep(1/60);
     }
 
-    createScene() : void {
+    createScene(): void {
         // Create a basic BJS Scene object.
         this._scene = new BABYLON.Scene(this._engine);
 
-        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), this._scene);
+        // Attach the physics engine to the scene
+        this._scene.enablePhysics(new BABYLON.Vector3(0, 0, 0), this._physicsEngine);
+        // Create a FreeCamera, and set its position to (x:0, y:15, z:-40).
+        this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 15, -40), this._scene);
         // Target the camera to scene origin.
         this._camera.setTarget(BABYLON.Vector3.Zero());
         // Attach the camera to the canvas.
@@ -30,25 +33,12 @@ class Game {
         // Create a basic light, aiming 0,1,0 - meaning, to the sky.
         this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this._scene);
 
-        // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
-        let sphere = BABYLON.MeshBuilder.CreateSphere(
-            'sphere',
-            {segments: 16, diameter: 2},
-            this._scene
-        );
-
-        // Move the sphere upward 1/2 of its height.
-        sphere.position.y = 1;
-
-        // Create a built-in "ground" shape.
-        let ground = BABYLON.MeshBuilder.CreateGround(
-            'ground',
-            {width: 6, height: 6, subdivisions: 2},
-            this._scene
-        );
+        // Create a couple of test planets
+        let planet1 = new TestSphere(this._scene, 3, 1, 11);
+        let planet2 = new TestSphere(this._scene, 1, 3, 6);
     }
 
-    doRender() : void {
+    doRender(): void {
         // Run the render loop.
         this._engine.runRenderLoop(() => {
             this._scene.render();
---
+++ stellar/object.ts
@@ -0,0 +1,30 @@
+class TestSphere {
+    private _phase: number;
+    private _speed: number;
+    private _orbitRadius: number;
+    private _sphere: BABYLON.Mesh;
+
+    constructor(scene: BABYLON.Scene, radius: number, speed: number, orbitRadius: number) {
+        this._phase = 0;
+        this._speed = speed;
+        this._orbitRadius = orbitRadius;
+
+        // Create a built-in "sphere" shape; with 16 segments.
+        this._sphere = BABYLON.MeshBuilder.CreateSphere(
+            'sphere',
+            {segments: 16, diameter: radius},
+            scene
+        );
+
+        // Bind fixedUpdate to be called every physics tick
+        scene.onBeforeStepObservable.add(() => this.fixedUpdate());
+    }
+
+    fixedUpdate(): void {
+        // Update phase.
+        this._phase = (this._phase + this._speed) % 360;
+        // Update position.
+        this._sphere.position.x = Math.cos(this._phase / 180 * Math.PI) * this._orbitRadius;
+        this._sphere.position.z = Math.sin(this._phase / 180 * Math.PI) * this._orbitRadius;
+    }
+}
