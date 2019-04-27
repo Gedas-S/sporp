ff26b3a87598870f6ffa60bb705c91b23dab4756
Gedas <g.simaitis@gmail.com>
2019-04-27 21:30:44 +0300

    add star glow

--- stellar/star.ts
+++ stellar/star.ts
@@ -2,7 +2,7 @@ class Star {
     private _sphere: BABYLON.Mesh;
     private _menu: StarMenu;
     private _material: BABYLON.StandardMaterial;
-    private _selectedColor: BABYLON.Color3 = BABYLON.Color3.Red();
+    private _glow: BABYLON.GlowLayer;
 
     constructor(
         private _scene: BABYLON.Scene,
@@ -23,8 +23,12 @@ class Star {
         var texture = new BABYLON.FireProceduralTexture("texture", 1024, _scene);
         texture.speed = new BABYLON.Vector2(0.02, 0.02);
         this._material.diffuseTexture = texture;
+        this._material.emissiveTexture = texture;
         this._material.emissiveColor = this.color;
         this._sphere.material = this._material;
+
+        this._glow = new BABYLON.GlowLayer("SunGlow", this._scene);
+        this._glow.addIncludedOnlyMesh(this._sphere);
 
         this._menu = new StarMenu(this._ui, this);
 
@@ -33,14 +37,18 @@ class Star {
     }
 
     static Random(scene: BABYLON.Scene, ui: BABYLON.GUI.AdvancedDynamicTexture, name: string): Star {
-        let color = Math.random();
         return new Star(
             scene,
             ui,
             Math.random() * 5 + Math.random() * 5 + 2,
             name,
-            new BABYLON.Color3(color + 0.5, 1, color % 0.5 + 0.5),
+            Star.RandomStarColor(),
         )
+    }
+
+    static RandomStarColor(): BABYLON.Color3 {
+        let color = Math.random();
+        return new BABYLON.Color3(color + 0.5, 1, color % 0.5 + 0.5)
     }
 
     click(eventData: BABYLON.PointerInfo): void {
@@ -54,32 +62,28 @@ class Star {
     enlarge(): void {
         this._radius += 1;
         this._scene.removeMesh(this._sphere);
+        this._glow.removeIncludedOnlyMesh(this._sphere);
         this._sphere = BABYLON.MeshBuilder.CreateSphere(
             'sphere',
             {segments: 16, diameter: this._radius},
             this._scene
         );
         this._sphere.material = this._material;
+        this._glow.addIncludedOnlyMesh(this._sphere);
     }
 
     colorize(): void {
-        this._radius += 1;
-        this._scene.removeMesh(this._sphere);
-        this._sphere = BABYLON.MeshBuilder.CreateSphere(
-            'sphere',
-            {segments: 16, diameter: this._radius},
-            this._scene
-        );
-        this._sphere.material = this._material;
+        this.color = Star.RandomStarColor()
+        this._material.emissiveColor = this.color;
     }
 
     select(): void {
-        this._material.emissiveColor = this._selectedColor;
+        this._glow.intensity = 1.8;
         this._menu.show();
     }
 
     deselect(): void {
-        this._material.emissiveColor = this.color;
+        this._glow.intensity = 1;
         this._menu.hide();
     }
 }
