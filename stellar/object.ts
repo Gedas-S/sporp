01ded0735a02e15dc0da3f220d3a6a0b45fd913a
Gedas <g.simaitis@gmail.com>
2019-04-27 15:34:09 +0300

    selectable spheres

--- stellar/object.ts
+++ stellar/object.ts
@@ -1,10 +1,12 @@
 class TestSphere {
-    private _phase: number;
+    private _phase: number = 0;
     private _sphere: BABYLON.Mesh;
+    private _selected: boolean = false;
+    private _material: BABYLON.StandardMaterial;
+    private _color: BABYLON.Color3 = BABYLON.Color3.Black();
+    private _selectedColor: BABYLON.Color3 = BABYLON.Color3.Red();
 
     constructor(private _scene: BABYLON.Scene, private _radius: number, private _speed: number, private _orbitRadius: number) {
-        this._phase = 0;
-
         // Create a built-in "sphere" shape; with 16 segments.
         this._sphere = BABYLON.MeshBuilder.CreateSphere(
             'sphere',
@@ -12,10 +14,16 @@ class TestSphere {
             this._scene
         );
 
+        this._material = new BABYLON.StandardMaterial("PlanetMaterial", this._scene);
+        this._sphere.material = this._material;
+        this._material.emissiveColor = this._color;
+
         // Bind fixedUpdate to be called every physics tick
         this._scene.onBeforeStepObservable.add(() => this.fixedUpdate());
         // Bind update to be called every frame
         this._scene.onBeforeRenderObservable.add(() => this.update());
+        // Bind click function (mask 32 is click).
+        this._scene.onPointerObservable.add((data, state) => this.click(data, state), 32);
     }
 
     fixedUpdate(): void {
@@ -29,5 +37,23 @@ class TestSphere {
         // Update mesh position.
         this._sphere.position.x = Math.cos(this._phase / 180 * Math.PI) * this._orbitRadius;
         this._sphere.position.z = Math.sin(this._phase / 180 * Math.PI) * this._orbitRadius;
+    }
+
+    click(eventData: BABYLON.PointerInfo, eventState: BABYLON.EventState): void {
+        if (eventData.pickInfo.pickedMesh == this._sphere) {
+            this.select();
+        } else if (this._selected) {
+            this.deselect();
+        }
+    }
+
+    select(): void {
+        this._selected = true;
+        this._material.emissiveColor = this._selectedColor;
+    }
+
+    deselect(): void {
+        this._selected = false;
+        this._material.emissiveColor = this._color;
     }
 }
