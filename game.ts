b6d5504b73f3df70eca86f183cc510a9ce971dc4
GabrieleJa <g.jagelaviciute@gmail.com>
2019-04-28 17:23:00 +0300

    input handling for W

--- game.ts
+++ game.ts
@@ -46,3 +46,12 @@ window.addEventListener('DOMContentLoaded', () => {
     // Start render loop.
     game.doRender();
 });
+
+window.addEventListener("keydown", (e: KeyboardEvent) => {
+    switch(e.keyCode){
+        case 87: 
+            //TODO: select Vytenis ship
+            break;
+    }
+    return true;
+});
